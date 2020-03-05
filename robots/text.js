c9bf610820d2563832ba9cb03cc493477a66b361
FelipeFriedrich <54379678+FelipeFriedrich@users.noreply.github.com>
2020-03-04 22:57:37 -0300

    update in Require ApyKey

--- robots/text.js
+++ robots/text.js
@@ -1,11 +1,11 @@
 const algorithmia = require('algorithmia');
-const {apyKey} = require('../credentials/credations');
+const algorithmiaApiKey = require('../credentials/credations.json').apiKey
 function robot(content){
     fetchContentFromWikipedia(content);
 
 
     async function fetchContentFromWikipedia(content){
-        const algorithmiaAuthenticated = algorithmia(apyKey);
+        const algorithmiaAuthenticated = algorithmia(algorithmiaApiKey);
         const wikipediaAlgorithn = algorithmiaAuthenticated.algo('web/WikipediaParser/0.1.2');
         const wikipediaResponde = await wikipediaAlgorithn.pipe(content.searchTerm);
         const wikipediaContent = wikipediaResponde.get();
